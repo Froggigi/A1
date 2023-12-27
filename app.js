5d0727118089e535c5b7922fbc0cf3843572b03a
Froggigi <asahigigi@gmail.com>
2023-12-27 22:20:38 +0800

    忘記分段commit 直接上傳

---
+++ app.js
@@ -0,0 +1,57 @@
+const express= require('express')
+const app = express()
+const restaurantsData = require("./restaurant.json").results
+// const Restaurant = require("./restaurant")
+
+const port = 3000
+
+// handlebars here
+const exphbs = require('express-handlebars')
+// set template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    Restaurant.find({})
+      .lean()
+      .then(restaurantsData => res.render('index', { restaurantsData }))
+      .catch(err => console.log(err))
+      
+})
+
+// setting static files
+app.use(express.static('public'))
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/restaurants/:restaurantId', (req,res) => {
+    const { restaurantId } = req.params
+    const restaurantData = restaurantsData.find(
+        data => data.id===Number(restaurantId)
+    )
+    res.render("show", { restaurantData})
+})
+
+// 新增餐廳頁面
+app.get('/restaurants/new', (req, res) => {
+    res.render('new')
+})
+
+app.get('/search', (req,res) => {
+    const keyword = req.query.keyword
+    const filterrestaurants = restaurantsData.filter(data =>{
+        return data.name.includes(keyword)||data.category.includes(keyword)
+    })
+    res.render('index', { restaurantsData:filterrestaurants,keyword:keyword })
+})
+
+// 新增餐廳資料
+app.post('/restaurants', (req, res) => {
+    Restaurant.create(req.body)
+      .then(() => res.redirect('/'))
+      .catch(err => console.error(err))
+})
+
+app.listen(port, () =>{
+    console.log(`Express is running on http://localhost:${port}`)
+})
